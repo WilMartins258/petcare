a6ec51b58b3a4404496bb967e39aaa891b26f116
WilMartins258 <willmartins258@gmail.com>
2023-05-11 17:13:04 -0300

    feat: mudando conexão para pool e removendo código que irá para index

--- server/src/db.js
+++ server/src/db.js
@@ -4,21 +4,12 @@ const mysql = require('mysql2');
  * Criando conexão com o banco de dados MySQL
  * Neste caso utilizando as configurações do mysql local que criei e a base de teste
  */
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
     host: 'localhost',
     port: 3306,
     user: 'root',
     password: '123456',
     database: 'petcare_homo'
 });
-
-// connection.connect((err) => {
-//     if (err) throw err;
-//     connection.query("SELECT * FROM usuario", (err, result) => {
-//       if (err) throw err;
-//       console.log(result);
-//     });
-// });
-
 
 module.exports = connection;

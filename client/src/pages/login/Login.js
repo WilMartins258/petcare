e0011d9ba56d4d0d31def881eecd6e0b320e61dd
WilMartins258 <willmartins258@gmail.com>
2023-05-13 13:43:16 -0300

    feat: trabalhando no componente de login

--- client/src/pages/login/Login.js
+++ client/src/pages/login/Login.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+// import axios from "axios";
+import api from "../../services/backend-service";
 
 function Login() {
 
@@ -13,19 +15,53 @@ function Login() {
      * Função executada ao clicar no botão de login
      * @param {evento de click} e 
      */
-    const handleLogin = async (e) => {
-        e.preventDefault();
+    // const handleLogin = async (e) => {
+    //     e.preventDefault();
 
-        console.log('login clicked')
-        console.log({email})
-        console.log({password})
-    }
+    //     console.log('login clicked')
+    //     console.log({email})
+    //     console.log({password})
+    // }
+
+    const handleLogin = async (values) => {
+      console.log({values});
+      console.log({email: values.email});
+      console.log({pw: values.password});
+      // await axios.post("http://localhost:3001/login", {
+      //   email: 'values.email',
+      //   password: 'values.password',
+      // }).then((response) => {
+      //   alert(response.data.msg);
+      // });
+      const objTest = {
+        email: "teste@teste.com.br",
+        password: "teste123"
+      }
+
+      const testing = await api.post("http://localhost:3001/login", objTest);
+
+      console.log({testing})
+    };
+  
+    // const handleRegister = (values) => {
+    //   Axios.post("http://localhost:3001/register", {
+    //     email: values.email,
+    //     password: values.password,
+    //   }).then((response) => {
+    //     alert(response.data.msg);
+    //     console.log(response);
+    //   });
+    // };
 
     return (
         <div className="div-form">
           <header className="App-header">
             <h2>Login</h2>
-            <form className='login-form'>
+            <form 
+            className='login-form'
+            onSubmit={handleLogin}
+            method="post"
+            target="http://localhost:3001/">
               <input 
                 type='email' 
                 name='email' 
@@ -51,4 +87,3 @@ function Login() {
 };
 
 export default Login;
-

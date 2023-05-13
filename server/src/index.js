cb12da52a47afdc5731adbd1daa41d243b55e98a
WilMartins258 <willmartins258@gmail.com>
2023-05-13 13:39:41 -0300

    fix: acertando configuração de cors nas rotas

---
+++ server/src/index.js
@@ -0,0 +1,36 @@
+// Importando módulos
+const express = require('express');
+const routes = require('./routes/routes.js');
+const cors = require('cors');
+
+// Importando conexão com banco??
+const db = require('./db.js');
+
+// Criando uma instância do express
+const app = express();
+
+// Indicando
+app.use(express.json());
+app.use(routes);
+// app.use(cors);
+app.use(cors());
+// 
+app.post('/login', cors(), (req, res) => {
+    console.log('LOGIN route')
+
+    
+    res.send('teste ok');
+});
+
+
+
+app.listen(3001, () => {
+    console.log('Listening on port 3001');
+})
+
+// db.query(
+//     "SELECT * FROM usuario",
+//     (err, results, fields) => {
+//         console.log(results);
+//     }
+// );
--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,14 +1,34 @@
 // Importando módulos
 const express = require('express');
+const cors = require('cors');
 
 const routes = express.Router();
 
-routes.post('/login', (req, res) => {
-    console.log('LOGIN')
+const app = express();
+
+app.use(cors());
+
+routes.get('/', cors(), (req, res) => {
+    res.send('Dados da página inicial');
+});
+
+// routes.post('/login', cors(), (req, res) => {
+//     console.log('LOGIN route')
+
+//     const { email, password} = req.body;
+    
+//     res.send(`email: ${email} \n senha: ${password}`);
+// });
+
+routes.post('/register', (req, res) => {
+    console.log('register route')
 
     const { email, password} = req.body;
-    
-    res.send(`email: ${email} \n senha: ${password}`);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // res.send(`email: ${email} \n senha: ${password}`);
 });
+
+
+
 
 module.exports = routes;

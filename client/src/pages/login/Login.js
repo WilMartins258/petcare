cd368d0ebe2251ba35bab78d51712dc5acabd2d2
WilMartins258 <willmartins258@gmail.com>
2023-05-23 19:49:12 -0300

    feat: criando versão estável do login sem estilo do projeto

--- client/src/pages/login/Login.js
+++ client/src/pages/login/Login.js
@@ -1,86 +1,92 @@
+import '../../reset.css';
+import './Login.css';
+
 import { useState } from "react";
-// import axios from "axios";
 import api from "../../services/backend-service";
 
 function Login() {
+  const input_email =  document.getElementById('input_email');
+  const input_password = document.getElementById('input_password');
+  const p_resposta = document.getElementById('resposta');
+  const [resposta, setResposta] = useState('');
 
-    /**
-     * states do React utilizados para armazenar e setar variáveis em um component
-     */
-    const [setEmail] = useState('');
-    // const [email, setEmail] = useState('');
-    const [setPassword] = useState('');
-    // const [password, setPassword] = useState('');
+    const ocultaResposta = () => {
+      console.log('ocultaResposta')
+      if (p_resposta && p_resposta.style.display !== 'none'){
+        p_resposta.style.display = "none";
+      }
+    }
 
+    const exibeResposta = () => {
+      console.log('exibeResposta')
+      if (p_resposta && p_resposta.style.display === 'none'){
+        p_resposta.style.display = "flex";
+      }
+    }
 
     /**
      * Função executada ao clicar no botão de login
-     * @param {evento de click} e 
      */
-    // const handleLogin = async (e) => {
-    //     e.preventDefault();
-
-    //     console.log('login clicked')
-    //     console.log({email})
-    //     console.log({password})
-    // }
-
     const handleLogin = async (values) => {
-      console.log({values});
-      console.log({email: values.email});
-      console.log({pw: values.password});
-      // await axios.post("http://localhost:3001/login", {
-      //   email: 'values.email',
-      //   password: 'values.password',
-      // }).then((response) => {
-      //   alert(response.data.msg);
-      // });
-      const objTest = {
-        email: "teste@teste.com.br",
-        password: "teste123"
-      }
+      values.preventDefault();
 
-      console.log('antes de chamar o back');
-      const testing = await api.post("/login", objTest);
-      console.log('após chamar o back');
-      console.log({testing})
+      exibeResposta();
+
+      if (!input_email || input_email.value.length === 0){
+        setResposta('Campo email vazio');
+      } else if (input_password.value.length === 0){
+        setResposta('Campo senha vazio');
+      } else {
+        const user_login_info = {
+          email: input_email.value,
+          password: input_password.value
+        }
+
+        console.log('antes de chamar o back \n');
+
+        const login = await api.post("/login", user_login_info);
+        
+        if (login.status === 200){
+          setResposta(login.data.msg);
+          console.log({login})
+        } else {
+          setResposta('E-mail ou senha incorretos');
+          console.log('E-mail ou senha incorretos');
+        }
+      }
     };
   
-    // const handleRegister = (values) => {
-    //   Axios.post("http://localhost:3001/register", {
-    //     email: values.email,
-    //     password: values.password,
-    //   }).then((response) => {
-    //     alert(response.data.msg);
-    //     console.log(response);
-    //   });
-    // };
-
     return (
         <div className="div-form">
           <header className="App-header">
             <h2>Login</h2>
-            <form 
-            className='login-form'
-            onSubmit={handleLogin}
-            method="post"
-            target="http://localhost:3001/">
+            <form className='login-form'>
               <input 
                 type='email' 
-                name='email' 
+                name='input_email' 
+                id='input_email' 
                 placeholder='Email' 
                 required
-                onChange={(e) => setEmail(e.target.value)}
+                onChange={ocultaResposta}
                 />
               <input 
                 type='password' 
-                name='password' 
+                name='input_password'
+                id='input_password' 
                 placeholder='Password' 
                 required
-                onChange={(e) => setPassword(e.target.value)}
+                onChange={ocultaResposta}
                 />
+                <p id="resposta" 
+                  style={{
+                    display: 'none', 
+                    marginLeft: '35%', 
+                    fontSize: '15px', 
+                    color: 'red'
+                  }}> 
+                  {resposta}
+                </p>
               <button 
-                type='submit' 
                 className='btn-login'
                 onClick={(e) => handleLogin(e)} >Login</button>
             </form>

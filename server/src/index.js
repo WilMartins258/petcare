efd9debbb79988488d19b8daeb5abfa01fdff861
WilMartins258 <willmartins258@gmail.com>
2023-05-25 21:34:00 -0300

    feat: limpando arquivo principal do back-end

--- server/src/index.js
+++ server/src/index.js
@@ -3,35 +3,16 @@ const express = require('express');
 const router = require('./routes/index.js');
 const cors = require('cors');
 
-// Importando conexão com banco??
+// Importando conexão com banco
 const db = require('./db.js');
 
 // Criando uma instância do express
 const app = express();
 
-// Indicando
 app.use(express.json());
 app.use(router);
 app.use(cors);
-// app.use(cors());
-
-// 
-// app.post('/login', cors(), (req, res) => {
-//     console.log('LOGIN route')
-
-    
-//     res.send('teste ok');
-// });
-
-
 
 app.listen(3001, () => {
     console.log('Listening on port 3001');
 })
-
-// db.query(
-//     "SELECT * FROM usuario",
-//     (err, results, fields) => {
-//         console.log(results);
-//     }
-// );

ed0754d3f091055b00bb9bcb02497f341923cf36
WilMartins258 <willmartins258@gmail.com>
2023-05-25 21:32:09 -0300

    feat: rota de login pronta

--- server/src/routes/login.js
+++ server/src/routes/login.js
@@ -7,48 +7,62 @@ router.get('/', (req, res) => {
     return res.status(200).send('GET Dados da página de login.');
 });
 
-router.post('/', async (req, res) => {
-    console.log('POST passou pelo /login');
-    console.log('req.body');
-    console.log({body: req.body});
-
-    const login_info = req.body;
+/**
+ * Query que busca os 
+ * @returns login_data
+ */
+const userLogin = async (user_email) => {
+    const connection = await db;
+    // console.log('user_emailuser_email: ', user_email);
 
     const login_query = "SELECT nome, email, senha, sexo FROM usuario WHERE email = ?";
-    const user_email = login_info.email;
 
-    db.query(login_query, [user_email],
-        (err, results) => {
-            if (!results || results.length === 0){
+    const [login_data] = await connection.query(login_query, user_email);
 
+    return login_data;
+}
+
+router.post('/', async (req, res) => {
+    console.log('POST passou pelo /login');
+
+    const login_info = req.body;
+    // console.log(login_info);
+
+    // Chamada para a consulta que busca pelo e-mail no nosso banco
+    const loginResult = await userLogin(login_info.email);
+
+    // console.log('\n\n\nOLHA O TESTE loginResult:');
+    // console.log(loginResult[0]);
+
+    if (!loginResult[0]){
+        return res.status(200).send({
+            login: false,
+            msg: 'Email não encontrado'
+        });
+    } else {
+    
+        // console.log('loginResult[0].senha:', loginResult[0].senha);
+        // console.log('login_info.password:', login_info.password);
+    
+        if (loginResult[0].senha === login_info.password){
+            if (loginResult[0].sexo === 'M') {
                 return res.status(200).send({
-                    login: false,
-                    msg: 'Email não encontrado'
+                    login: true,
+                    msg: `Bem-vindo, ${loginResult[0].nome}!`
                 });
             } else {
-
-                console.log('Email encontrado!');
-
-                console.log('login_info.password:', login_info.password);
-                console.log('results[0].senha:', results[0].senha);
-
-                if (login_info.password === results[0].senha){
-
-                    return res.status(200).send({
-                        login: true,
-                        msg: `Bem-vindo(a), ${results[0].nome}!`
-                    });
-                } else {
-
-                    return res.status(200).send({
-                        login: false,
-                        msg: 'Senha incorreta'
-                    });
-                }
+                return res.status(200).send({
+                    login: true,
+                    msg: `Bem-vinda, ${loginResult[0].nome}!`
+                });
             }
-        },
-    );
+        } else {
+            return res.status(200).send({
+                login: false,
+                msg: 'Senha incorreta'
+            });
+        }
+    }
 });
-
 
 module.exports = router;
